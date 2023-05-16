f9dcfd14435968063952c92dc6ab81da2e1ffa3a
Yehor <116878096+licht8@users.noreply.github.com>
2023-05-16 20:21:08 +0200

    Update check_and_install_bc.sh

--- functions/check_and_install_bc.sh
+++ functions/check_and_install_bc.sh
@@ -2,12 +2,12 @@
 
 # Function for checking and installing the bc package
 check_and_install_bc() {
-    if ! command -v bc 2>/dev/null ; then
+    if ! command -v bc >/dev/null 2>&1; then
         echo "The 'bc' package is not found. Installing the 'bc' package..."
-        if command -v dnf 2>/dev/null ; then
-            sudo dnf install -y bc 2>/dev/null
-        elif command -v yum 2>/dev/null ; then
-            sudo yum install -y bc 2>/dev/null
+        if command -v dnf >/dev/null 2>&1; then
+            sudo dnf install -y bc >/dev/null 2>&1
+        elif command -v yum >/dev/null 2>&1; then
+            sudo yum install -y bc >/dev/null 2>&1
         else
             echo "Failed to install the 'bc' package. Please install it manually."
             exit 1
